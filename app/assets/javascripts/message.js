ae6eb04d984cc4644dc67fbfbae4066e96540c78
mariannu0627 <mariannu0627@gmail.com>
2020-06-15 19:24:45 +0900

    200615_修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,32 +20,47 @@ $(function(){
                 </div>
               </div>`
           return html; //return文
-      };
-    }
+  } else {
+            var html =
+             `<div class="message" data-message-id=${message.id}>
+                <div class="message__info">
+                  <div class="message__info__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="message__info__date">
+                    ${message.date}
+                  </div>
+                </div>
+                <div class="message__text">
+                  <p class="lower-message__content">
+                    ${message.content}
+                  </p>
+                </div>
+              </div>`
+            return html;
+    };
+});
+
   $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action')
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-       })  
-       .done(function(data){ //非同期通信の結果として返ってくるデータが引数(data)に入る
-         var html = buildHTML(data); //return文の戻り先で、完成したHTMLを受け取る
-       })
-     })
-  };
-    .done(function(data){
-      var html = buildHTML(data);
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })  
+    .done(function(data){ //非同期通信の結果として返ってくるデータが引数(data)に入る
+      var html = buildHTML(data); //return文の戻り先で、完成したHTMLを受け取る
       $('.messages').append(html);
       $("form")[0].reset(); //form内を空にする記述
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
-      $('input').prop('disabled', false);
-      .fail(function() {
-        alert("メッセージ送信に失敗しました");
-    });
-  });
+      $('.messages').animate({ scrollTop:$('.messages')[0].scrollHeight});
+      $('.submit-btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    })
+  })
--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.time_zone = ‘Tokyo’
+# config.time_zone = ‘Tokyo’
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,7 +16,8 @@ module ChatSpace
       g.javascripts false
       g.helper false
       g.test_framework false
-      config.i18n.default_locale = :ja
     end
+      config.i18n.default_locale = :ja
+      config.time_zone = 'Asia/Tokyo'
   end
 end
